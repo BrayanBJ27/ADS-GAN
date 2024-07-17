61c1dc8eca33ddf0f983490346c43ea8144e5f28
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-17 14:23:33 -0500

    fix: :bug: Fix nlp model

--- models/nlp.py
+++ models/nlp.py
@@ -1,28 +1,12 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
-import torch
-
-# Cargar el modelo preentrenado y el tokenizador
-model_name = "gpt2"  # Puedes usar otros modelos como "gpt2-medium", "gpt2-large", etc.
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+from transformers import pipeline, set_seed
+import random
 
 def generate_text(prompt):
-    # Tokenizar el prompt
-    inputs = tokenizer.encode(prompt, return_tensors="pt")
-
-    # Crear una máscara de atención
-    attention_mask = torch.ones(inputs.shape, dtype=torch.long)
-
-    # Generar texto
-    outputs = model.generate(
-        inputs,
-        max_length=50,
-        num_return_sequences=1,
-        pad_token_id=tokenizer.eos_token_id,  # Configurar pad_token_id al eos_token_id
-        attention_mask=attention_mask,        # Configurar explicitamente la máscara de atención
-    )
-
-    # Decodificar el texto generado
-    text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    generator = pipeline('text-generation', model='gpt2')
+    set_seed(random.randint(1, 10000))  # Para variedad en las generaciones
+    result = generator(prompt, max_length=50, num_return_sequences=3)
     
-    return text
+    # Seleccionar la mejor respuesta (puedes implementar una lógica más sofisticada aquí)
+    best_response = max(result, key=lambda x: len(x['generated_text']))
+    
+    return best_response['generated_text'].strip()
