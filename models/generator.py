01c3fcf0de4820ad148bc7bc4bdc1e7d2caaf344
BrayanBJ27 <xavi_braian@hotmail.com>
2024-07-17 14:24:45 -0500

    fix: :bug: Fix generator model

--- models/generator.py
+++ models/generator.py
@@ -1,16 +1,12 @@
+import tensorflow as tf
 from tensorflow.keras.layers import Input, Reshape, Dropout, Dense, Flatten, BatchNormalization, Activation, ZeroPadding2D, LeakyReLU, UpSampling2D, Conv2D
 from tensorflow.keras.models import Sequential
 
 def build_generator(seed_size, channels):
     model = Sequential()
-    model.add(Input(shape=(seed_size,)))  # Usar Input(shape) en lugar de input_dim
-    model.add(Dense(15*15*256, activation="relu"))
-    model.add(Reshape((15, 15, 256)))
-
-    model.add(UpSampling2D())
-    model.add(Conv2D(256, kernel_size=3, padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
+    model.add(Input(shape=(seed_size,)))
+    model.add(Dense(30*30*256, activation="relu"))
+    model.add(Reshape((30, 30, 256)))
 
     model.add(UpSampling2D())
     model.add(Conv2D(256, kernel_size=3, padding="same"))
@@ -22,13 +18,16 @@ def build_generator(seed_size, channels):
     model.add(BatchNormalization(momentum=0.8))
     model.add(Activation("relu"))
 
-    if 470 > 1:
-        model.add(UpSampling2D(size=(15, 15)))
-        model.add(Conv2D(128, kernel_size=3, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Activation("relu"))
+    model.add(UpSampling2D())
+    model.add(Conv2D(64, kernel_size=3, padding="same"))
+    model.add(BatchNormalization(momentum=0.8))
+    model.add(Activation("relu"))
 
+    model.add(UpSampling2D(size=(2,2)))
     model.add(Conv2D(channels, kernel_size=3, padding="same"))
     model.add(Activation("tanh"))
+
+    # Crop to 470x470
+    model.add(tf.keras.layers.Cropping2D(cropping=((5, 5), (5, 5))))
 
     return model
